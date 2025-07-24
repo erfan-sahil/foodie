3644ce7ec17819238cacdc92fca7838f68f75c5f
erfan-sahil <erfansahil20@gmail.com>
2025-07-24 22:49:54 +0600

    database connected

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,16 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to Database:", err);
+  });
 
 const app = express();
 app.use(express.json());
