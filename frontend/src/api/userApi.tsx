aa9edd8cd1f72f55a6311047e668744884f2486b
erfan-sahil <erfansahil20@gmail.com>
2025-07-28 22:39:42 +0600

    create user api req using react query

---
+++ frontend/src/api/userApi.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useMutation, QueryClient } from "@tanstack/react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type CreateUserRequest = {
+  auth0Id: string;
+  email: string;
+};
+
+export const useCreateUser = () => {
+  const queryClient = new QueryClient();
+
+  const createUserRequest = async (user: CreateUserRequest) => {
+    const response = await axios.post(`${API_BASE_URL}/api/v1/user`, user, {
+      // headers: {
+      //   'Content-Type': 'application/json',
+      // },
+    });
+    if (!response.data) {
+      throw new Error("Failed to create user");
+    }
+  };
+
+  const {
+    mutate: createUser,
+    isError,
+    isSuccess,
+  } = useMutation({
+    mutationFn: createUserRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+  });
+
+  return {
+    createUser,
+    isError,
+    isSuccess,
+  };
+};
