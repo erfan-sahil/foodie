275c21bbb31dd83df37d62062838dd50f4c11eda
erfan-sahil <erfansahil20@gmail.com>
2025-07-29 20:53:42 +0600

    add jwt check on backend

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import createUser from "../controllers/userController";
+import { jwtCheck } from "../middlewares/auth";
 
 const userRoute = express.Router();
 
-userRoute.post("/", createUser);
+userRoute.post("/", jwtCheck, createUser);
 
 export default {
   userRoute,
