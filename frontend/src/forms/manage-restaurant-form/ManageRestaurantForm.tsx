137544f9bccb40bff94c95a9d55baac7938cf855
erfan-sahil <erfansahil20@gmail.com>
2025-08-08 07:07:20 +0600

    create deatils section of restaurant form

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -2,6 +2,7 @@ import { Form } from "@/components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import DetailsSection from "./DetailsSectrion";
 
 const formSchema = z.object({
   restaurantName: z
@@ -51,7 +52,12 @@ const ManageRestaurantForm = ({ onSave, isPending }: Props) => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}></form>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+      </form>
     </Form>
   );
 };
