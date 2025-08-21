d2a68977e61452dd09360acf9a70aebd37dd5e34
erfan-sahil <erfansahil20@gmail.com>
2025-08-21 23:46:08 +0600

    initial setup of custom checkbox

---
+++ frontend/src/forms/manage-restaurant-form/CuisineCheckbox.tsx
@@ -0,0 +1,26 @@
+import { FormControl, FormItem } from "@/components/ui/form";
+import { Checkbox } from "@radix-ui/react-checkbox";
+import type { ControllerRenderProps, FieldValues } from "react-hook-form";
+
+type Props = {
+  cuisine: string;
+  field: ControllerRenderProps<FieldValues, "cuisines">;
+};
+
+const CuisineCheckbox = ({ cuisine, field }: Props) => {
+  return (
+    <FormItem
+      className="flex flex-row items-center space-x-1 space-y-0
+  mt-2"
+    >
+      <FormControl>
+        <Checkbox
+          className="bg-white"
+          checked={field.value.includes(cuisine)}
+        />
+      </FormControl>
+    </FormItem>
+  );
+};
+
+export default CuisineCheckbox;
--- frontend/src/forms/manage-restaurant-form/CuisinesSection.tsx
+++ frontend/src/forms/manage-restaurant-form/CuisinesSection.tsx
@@ -1,8 +1,14 @@
-import { FormDescription, FormField, FormItem } from "@/components/ui/form";
+import {
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { cuisineList } from "@/config/restaurant-options-config";
 import { useFormContext } from "react-hook-form";
 
 const CuisinesSection = () => {
-  const { control } = useFormContext;
+  const { control } = useFormContext();
   return (
     <div className="space-y-2">
       <div>
@@ -17,7 +23,12 @@ const CuisinesSection = () => {
         name="cuisines"
         render={({ field }) => (
           <FormItem className="flex flex-col gap-2">
-            <div className=""></div>
+            <div className="grid md:grid-cols-5 gap-1">
+              {cuisineList.map((cuisineItem) => (
+                <CuisineCheckBox cuisine={cuisineItem} field={field} />
+              ))}
+            </div>
+            <FormMessage />
           </FormItem>
         )}
       />
