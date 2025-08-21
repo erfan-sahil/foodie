233dc2951b618685b1bf3677157af5af2b1eaff1
erfan-sahil <erfansahil20@gmail.com>
2025-08-21 18:21:27 +0600

    add cuisines list

---
+++ frontend/src/config/restaurant-options-config.ts
@@ -0,0 +1,14 @@
+export const cuisineList = [
+  "Traditional Bangladeshi",
+  "Indian",
+  "Chinese",
+  "Thai",
+  "Fast Food / Burgers",
+  "Cafe & Bakery",
+  "Grill & BBQ",
+  "Street Food",
+  "Seafood Special",
+  "Vegetarian / Vegan",
+  "Desserts & Sweets",
+  "Juice & Smoothie Bar",
+];
