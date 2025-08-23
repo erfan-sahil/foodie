9e22ff48a5460df06e9f26e5aad51e764200eb1a
erfan-sahil <erfansahil20@gmail.com>
2025-08-23 22:54:20 +0600

    connect api with formSubmit

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,11 @@
+import { useCreateRestaurant } from "@/api/restaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
+  const { createRestaurant, isPending } = useCreateRestaurant();
   return (
     <>
-      <ManageRestaurantForm />
+      <ManageRestaurantForm onSave={createRestaurant} isPending={isPending} />
     </>
   );
 };
