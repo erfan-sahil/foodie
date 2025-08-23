91552ea8f7fce8db73d913f18a92171239fae9f2
erfan-sahil <erfansahil20@gmail.com>
2025-08-23 20:23:51 +0600

    create image section

--- frontend/src/forms/manage-restaurant-form/ImageSection.tsx
+++ frontend/src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,5 +1,50 @@
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useFormContext } from "react-hook-form";
+
 const ImageSection = () => {
-  return <div></div>;
+  const { control } = useFormContext();
+  return (
+    <div className="space-y-2">
+      <div>
+        <h2 className="text-2xl font-bold">Image</h2>
+        <FormDescription>
+          Add an image that will be displayed on you restaurant listing in the
+          search results. Adding a new image will overwrite the existing image.
+        </FormDescription>
+      </div>
+
+      <div className="flex flex-col gap-8 w-[50%]">
+        <FormField
+          control={control}
+          name="imaageFile"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="bg-white"
+                  type="file"
+                  accept=".jpg, .jpeg , .png"
+                  onChange={(event) => {
+                    field.onChange(
+                      event.target.files ? event.target.files[0] : null
+                    );
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ImageSection;
