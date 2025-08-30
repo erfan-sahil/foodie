a9ffc2426decb9f193e8f5e102ae4e60c7e250e3
erfan-sahil <erfansahil20@gmail.com>
2025-08-30 23:44:41 +0600

    add types on fetch func

--- frontend/src/api/restaurantApi.tsx
+++ frontend/src/api/restaurantApi.tsx
@@ -1,19 +1,24 @@
+import type { RestaurantSearchResponse } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useSearchRestaurants = (city?: string) => {
+export const useSearchRestaurants = (
+  city?: string
+): Promise<RestaurantSearchResponse> => {
   const createSearchRequests = async () => {
     const response = await axios.get(
-      `${API_BASE_URL}/api/restaurants?city=${city}`
+      `${API_BASE_URL}/api/restaurant/search/${city}`
     );
+
+    console.log(response);
 
     if (!response.data) {
       throw new Error("Failed to fetch restaurants");
     }
-
-    return response.data;
+    console.log("restaurant search data", response.data);
+    return response;
   };
 
   const { data: results, isPending } = useQuery({
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,6 +1,8 @@
+import { useSearchRestaurants } from "@/api/restaurantApi";
 import { useParams } from "react-router-dom";
 
 export default function SearchPage() {
   const { city } = useParams();
+  const { results, isPending } = useSearchRestaurants(city);
   return <div>User searched for {city}</div>;
 }
