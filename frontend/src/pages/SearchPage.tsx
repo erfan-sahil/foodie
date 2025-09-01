6de218123091893883d8ba596d22f78692f8e3a6
erfan-sahil <erfansahil20@gmail.com>
2025-09-01 21:23:48 +0600

    create inital setup of search restaurant

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,11 +1,35 @@
 import { useSearchRestaurants } from "@/api/restaurantApi";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
+import SearchBar from "@/components/shared/SearchBar";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type SearchState = {
+  searchQuery: string;
+};
 
 export default function SearchPage() {
   const { city } = useParams();
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+  });
+
   const { results, isPending } = useSearchRestaurants(city);
+
+  const setSearchQuey = (searchFormdata: SearchState) => {
+    setSearchState((prev) => ({
+      ...prev,
+      searchQuery: searchFormdata.searchQuery,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prev) => ({
+      ...prev,
+      searchQuery: "",
+    }));
+  };
 
   if (!results?.data || !city) {
     return <span> No results found</span>;
@@ -14,13 +38,16 @@ export default function SearchPage() {
   if (isPending) {
     return <span> Loading...</span>;
   }
-
-  console.log(results);
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
       <div id="cuisines-list">insert cuisines list here</div>
 
       <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          onSubmit={setSearchQuey}
+          placeHolder="Search by cuisine or Restaurant name"
+          onReset={resetSearch}
+        />
         <SearchResultInfo total={results.pagination.total} city={city} />
 
         {results.data.map((restaurant, index) => (
